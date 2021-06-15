ac81910db95febf7e82b4835cafc7dbf94338f72
anisanurfadila <anisanfd925@gmail.com>
2021-06-15 09:31:30 +0700

    akir

--- detail.php
+++ detail.php
@@ -44,7 +44,7 @@
                         <p>Nama : <?php echo $data['nama'] ?></p>
                         <p>NIM : <?php echo $data['nim'] ?></p>
                         <p>Alamat : <?php echo $data['alamat'] ?></p>
-                        <button class="btn btn-primary">Cetak</button>
+                        <a href="print.php?id=<?php echo $data['id']; ?>"  class="btn btn-primary">Cetak</a>
                     </div>
                 </div>
             </div>
